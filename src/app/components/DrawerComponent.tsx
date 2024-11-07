41b37fa8538a98fb7112f48d86da12db64341b5a
Adam Stępień <vesemir2k@gmail.com>
2024-11-07 15:25:49 +0100

    ✨ (DrawerComponent.tsx): Add a new DrawerComponent to handle the drawer functionality in the application layout 🔧 (layout.tsx): Refactor RootLayout component to use the newly added DrawerComponent for managing the drawer functionality in the application layout

---
+++ src/app/components/DrawerComponent.tsx
@@ -0,0 +1,113 @@
+import Divider from "@mui/material/Divider"
+import Drawer from "@mui/material/Drawer"
+import InboxIcon from "@mui/icons-material/MoveToInbox"
+import List from "@mui/material/List"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import MailIcon from "@mui/icons-material/Mail"
+import Toolbar from "@mui/material/Toolbar"
+
+type DrawerComponentProps = {
+  window?: () => Window
+  setIsClosing: (isClosing: boolean) => void
+  setMobileOpen: (mobileOpen: boolean) => void
+  mobileOpen: boolean
+  drawerWidth: number
+}
+
+export default function DrawerComponent({
+  window,
+  setIsClosing,
+  setMobileOpen,
+  mobileOpen,
+  drawerWidth,
+}: DrawerComponentProps) {
+  const handleDrawerClose = () => {
+    setIsClosing(true)
+    setMobileOpen(false)
+  }
+
+  const handleDrawerTransitionEnd = () => {
+    setIsClosing(false)
+  }
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined
+
+  const drawer = (
+    <>
+      <Toolbar />
+      <Divider />
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem
+            key={text}
+            disablePadding
+          >
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem
+            key={text}
+            disablePadding
+          >
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  )
+
+  return (
+    <>
+      <Drawer
+        container={container}
+        variant="temporary"
+        open={mobileOpen}
+        onTransitionEnd={handleDrawerTransitionEnd}
+        onClose={handleDrawerClose}
+        ModalProps={{
+          keepMounted: true,
+        }}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+      >
+        {drawer}
+      </Drawer>
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: drawerWidth,
+          },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </>
+  )
+}

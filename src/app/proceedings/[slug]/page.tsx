af64d3b9ee54c8d1927e2227b3c1c0864dd6739a
Adam Stępień <vesemir2k@gmail.com>
2024-11-13 23:22:33 +0100

    ✨ (layout.tsx): Add TranscriptsListProvider to provide context for transcripts list in the layout component 📝 (page.tsx): Add useTranscriptsList hook to fetch and display transcripts list in the Proceeding component

--- src/app/proceedings/[slug]/page.tsx
+++ src/app/proceedings/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { useAppBar } from "@/app/context/AppBarContext"
 import { useEffect } from "react"
 import { useParams } from "next/navigation"
 import { useProceeding } from "@/app/context/ProceedingContext"
+import { useTranscriptsList } from "@/app/context/TranscriptsList"
 
 type ProceedingPost = {
   params?: { slug: string | string[] | undefined }
@@ -12,6 +13,8 @@ type ProceedingPost = {
 export default function Proceeding({}: ProceedingPost) {
   const { setTitle } = useAppBar()
   const { ProceedingFetchData, ProceedingData } = useProceeding()
+  const { TranscriptsListFetchData, TranscriptsListData } = useTranscriptsList()
+
   const params = useParams()
   const slug = typeof params.slug === "string" ? params.slug : undefined
 
@@ -27,5 +30,17 @@ export default function Proceeding({}: ProceedingPost) {
     }
   }, [ProceedingFetchData, slug])
 
-  return <div>{ProceedingData?.title}</div>
+  useEffect(() => {
+    if (slug) {
+      TranscriptsListFetchData(slug, "2023-11-13")
+    }
+  }, [TranscriptsListFetchData, slug])
+
+  return (
+    <div>
+      {TranscriptsListData?.statements.map((item) => (
+        <h3 key={item.num}>{item.name}</h3>
+      ))}
+    </div>
+  )
 }
